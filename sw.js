719201829c1eba4a191b1d02cef62936ad38b4e8
JPMacedo1973 <jpmacedo.bvs@gmail.com>
2026-09-15 23:19:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-const C='treino-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C = 'treino-v3';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(C).then(c =>
+      c.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ])
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== C)
+          .map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
